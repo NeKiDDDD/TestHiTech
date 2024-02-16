ac31901c6e3534332d031ca4286edb79f3d12e53
lax3331 <la2life1234@gmail.com>
2024-02-16 19:09:52 +0400

    Initial Commit

---
+++ TestHiTech/TestHiTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHiTechApp.swift
+//  TestHiTech
+//
+//  Created by Nikita Kuznetsov on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestHiTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
